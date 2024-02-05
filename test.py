a9ca5c1f7c4fecb81af1f5373a65188b64276366
Ida <202308610@post.au.dk>
2024-02-05 10:40:23 +0100

    fixed

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 import os
+from os.path import expanduser
 
 def create_empty_file(folder_path, file_name):
 
@@ -7,13 +8,14 @@ def create_empty_file(folder_path, file_name):
     file_path = os.path.join(folder_path, file_name)
 
     # Create an empty file
-    with open(file_path, 'w') as file:
-        pass
+    # with open(file_path, 'w') as file:
+    #    pass
+    file = open(expanduser("~")+file_path,'w')
 
     print(f"Empty file '{file_name}' created in '{folder_path}'.")
 
 # Example usage:
-folder_path = 'C:/Users/idavi/Documents/Uni/2.semester/Computerteknologiprojekt1'
+folder_path = '/Desktop'
 file_name = 'Computerteknologiprojekt1.txt'
 
 create_empty_file(folder_path, file_name)
